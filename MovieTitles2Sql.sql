0d2949a871f8415a65dca4552784cec447561ab5
alice8347 <alice8347@hotmail.com>
2015-08-17 09:52:34 -0400

    SQL

---
+++ MovieTitles2Sql.sql
@@ -0,0 +1,11 @@
+CREATE TABLE movieTitleGenerator (
+    id INTEGER PRIMARY KEY, 
+    adj VARCHAR(100), 
+    noun VARCHAR(100)
+);
+
+CREATE TABLE movies (
+	id INTEGER PRIMARY KEY, 
+	name VARCHAR(100), 
+	discription VARCHAR(200)
+);
